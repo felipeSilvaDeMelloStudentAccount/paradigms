949b31fb6389dc742a0a1f534a3689295db6a29c
felipeSilvaDeMelloStudentAccount <146777952+felipeSilvaDeMelloStudentAccount@users.noreply.github.com>
2024-11-24 14:01:42 +0000

    Part 2 - Hight

--- assignment1/part2.pl
+++ assignment1/part2.pl
@@ -1,7 +1,25 @@
 % Print a row
 print_row([]).
-print_row([Element]):- write('|'),write(Element), write('|').
+print_row([Element]):- write('|'), write(Element), write('|').
 print_row([Element | NextElement]):- write('|'), write(Element), print_row(NextElement).
 
+% Print the blocks
 print_status([]).
 print_status([Row | NextRow]):- print_row(Row), nl, print_status(NextRow).
+
+
+
+% Find the block height in a pile.
+high_in_pile([Block | _], Block, AccumulateHeight, AccumulateHeight).
+high_in_pile([_ | NextBlock], Block, AccumulateHeight, Height) :-
+    CurrentHeight is AccumulateHeight + 1,
+    high_in_pile(NextBlock, Block, CurrentHeight, Height).
+
+% Main predicate.
+high([], _, _).
+high([Pile | NextPile], Block, Height) :-
+    % Find the block height in the pile.
+    (high_in_pile(Pile, Block, 0, Height)
+    ; % If not found, Search in the rest of the piles.
+    high(NextPile, Block, Height)).
+    
