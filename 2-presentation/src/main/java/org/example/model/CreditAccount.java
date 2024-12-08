e9f779178e78ca9a1edec0e58affa5eb45796e64
felipeSilvaDeMelloStudentAccount <146777952+felipeSilvaDeMelloStudentAccount@users.noreply.github.com>
2024-12-08 17:15:42 +0000

    Presentation 2 - Code before refactoring

---
+++ 2-presentation/src/main/java/org/example/model/CreditAccount.java
@@ -0,0 +1,10 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class CreditAccount implements Account {
+    private double balance;
+    @Override
+    public double getBalance() {return balance;}
+}
---
+++ 2-presentation/src/main/java/org/example/model/MortgageAccount.java
@@ -0,0 +1,10 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class MortgageAccount implements Account {
+    private double balance;
+    @Override
+    public double getBalance() {return balance;}
+}
